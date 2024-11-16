7a1aae224751dbe6044fd73a5ac54b8fc15ae0cf
SL33PiNg <nook3920@gmail.com>
2024-11-16 23:06:22 +0700

    add Histoire

--- base/nuxt.config.ts
+++ base/nuxt.config.ts
@@ -3,29 +3,34 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', "vuetify-nuxt-module", "@nuxtjs/i18n"], 
+  modules: ['@pinia/nuxt', "vuetify-nuxt-module", "@nuxtjs/i18n"],
   i18n: {
-    langDir: "assets/locales/", 
+    langDir: "assets/locales/",
     locales: [
       { name: "English", code: "en", iso: "en-US", file: "en.json" },
       { name: "ไทย", code: "th", iso: "th-TH", file: "th.json" },
-    ], 
-    defaultLocale: 'th',    
+    ],
+    defaultLocale: 'th',
     detectBrowserLanguage: false,
   },
   runtimeConfig: {
     // Private variables (only available on server-side)
-    secretApiKey: process.env.SECRET_API_KEY,
-    
-    // Public variables (available on client-side via `useRuntimeConfig()`)
-    public: {
-      apiBaseUrl: process.env.API_BASE_URL,
-      appName: process.env.PUBLIC_APP_NAME
-    }
+    // secretApiKey: process.env.SECRET_API_KEY,
+
+    // // Public variables (available on client-side via `useRuntimeConfig()`)
+    // public: {
+    //   apiBaseUrl: process.env.API_BASE_URL,
+    //   appName: process.env.PUBLIC_APP_NAME
+    // }
+    runtimeConfig: {
+      public: {
+        configFromNuxt: 'test',
+      },
+    },
   },
   vuetify: {
     vuetifyOptions: {
-      
+
     }
   },
   build: {
