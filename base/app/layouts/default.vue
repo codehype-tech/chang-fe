d96129e10d091c0ffcff9a5c851246d793b94295
taloengrat.p <arm.taloengrat.superdev@gmail.com>
2024-11-20 11:11:54 +0700

    feature : init base layout an example on customer repo page

---
+++ base/app/layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="default-layout">
+    <div style="flex: 1">
+      <slot />
+    </div>
+    <v-divider></v-divider>
+    <div style="display: flex; flex-direction: column; position: relative">
+      <slot name="bottom" />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+// Optionally include any setup logic here
+</script>
+<style lang="scss" scoped>
+.default-layout {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  box-sizing: border-box;
+  scrollbar-width: none;
+}
+</style>
---
+++ customer/app/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtLayout>
+    CONTENT
+    <template v-slot:bottom>
+      <div class="ma-8">
+        <VBtn block color="primary">
+          <label for=""> Let’s get started </label>
+        </VBtn>
+      </div>
+    </template>
+  </NuxtLayout>
+</template>
+
+<script lang="ts" setup></script>
